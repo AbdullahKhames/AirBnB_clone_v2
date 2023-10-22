0e59c981e96ef8a4dc25870ff6e70da14f75b614
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-10-22 22:55:46 +0300

    sorted

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,7 +3,6 @@
 from flask import Flask, render_template, url_for
 from models import storage
 from models.state import State
-from models.city import City
 from os import getenv
 app = Flask(__name__)
 
@@ -19,12 +18,14 @@ def list_cities_states():
     """listing all states"""
     states = sorted(storage.all(State).values(), key=lambda state: state.name)
     cities = {}
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        for state in states:
-            cities[state.id] = sorted(state.cities, key=lambda city: city.name)
-    elif getenv('HBNB_TYPE_STORAGE') == 'file':
-        for state in states:
-            cities[state.id] = sorted(state.cities, key=lambda city: city.name)
+    # if getenv('HBNB_TYPE_STORAGE') == 'db':
+    #     for state in states:
+    #         cities[state.id] = sorted(state.cities, key=lambda city: city.name)
+    # elif getenv('HBNB_TYPE_STORAGE') == 'file':
+    #     for state in states:
+    #         cities[state.id] = sorted(state.cities, key=lambda city: city.name)
+    for state in states:
+        cities[state.id] = sorted(state.cities, key=lambda city: city.name)
     return render_template('8-cities_by_states.html',
                            states=states, cities=cities)
 
